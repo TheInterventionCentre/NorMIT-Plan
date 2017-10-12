5b6eb4ab542c7689f4d527edf23416ba7086645e
Louise Oram <lcoram@gmail.com>
2017-10-12 11:48:22 +0200

    Reviewed / changed licence statements in files to be consistent (except one ITK file where the ITK license was kept). Closes #55

--- ResectionPlanning/Logic/vtkSlicerResectionPlanningLogic.h
+++ ResectionPlanning/Logic/vtkSlicerResectionPlanningLogic.h
@@ -32,7 +32,6 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   =========================================================================*/
 
-
 #ifndef __vtkSlicerResectionPlanningLogic_h
 #define __vtkSlicerResectionPlanningLogic_h
 
