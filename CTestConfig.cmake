f1d1dae5f3d861d524df241159aede9d7a1d874f
Rafael Palomar <rafael.palomar@rr-research.no>
2017-10-17 10:00:24 +0200

    Changing the CDash Board

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -6,10 +6,10 @@
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
 
-set(CTEST_PROJECT_NAME "NorMitPlanExt")
+set(CTEST_PROJECT_NAME "NorMIT-Plan Extension")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=NorMitPlanExt")
+set(CTEST_DROP_LOCATION "/submit.php?project=NorMit-Plan+Extension")
 set(CTEST_DROP_SITE_CDASH TRUE)
